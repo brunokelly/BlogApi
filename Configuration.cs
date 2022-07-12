d68c159920f4250a5d0843c9788663fc5e28b54e
brunokelly <brunobuch00@gmail.com>
2022-07-12 20:44:52 -0300

    Creating token

---
+++ Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogApi;
+
+public static class Configuration
+{
+    public static string JwtKey { get; set; } = "jGQCzfud80KsR+lpw/bn/Q==";
+    public static string JwtSecret { get; set; }
+
+}
+
