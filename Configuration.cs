9da48fec624d99863bc88b31233d9a157f96d9f7
brunokelly <brunobuch00@gmail.com>
2022-07-29 22:39:26 -0300

    Added email service

--- Configuration.cs
+++ Configuration.cs
@@ -1,4 +1,5 @@
-﻿namespace BlogApi;
+﻿using System.Net.Mail;
+namespace BlogApi;
 
 public static class Configuration
 {
@@ -6,6 +7,14 @@ public static class Configuration
 
     public static string ApiKeyName = "api_key";
     public static string ApiKey = "api_jGQCzfud80KsR+lpw/bn/Q==";
+    public static SmtpConfiguration Smtp = new();
+    public class SmtpConfiguration
+    {
+        public string Host { get; set; }
+        public int Port { get; set; } = 25;
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
 
 }
 
